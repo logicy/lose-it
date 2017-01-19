20ae8ef3ea28f42cb754a4686e496008f59a3a2a
sushiljainam <sushiljain1993@gmail.com>
2017-01-19 14:50:58 +0530

    started bot

---
+++ bot.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Sushil Jain
+* @Date:   2017-01-19 14:48:59
+* @Last Modified by:   sushiljainam
+* @Last Modified time: 2017-01-19 14:50:33
+*/
+
+'use strict';
+
+var Bot = function(name, smartness){
+	
+}
